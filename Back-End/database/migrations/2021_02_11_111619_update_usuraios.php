c0ae350d3d5c9b994fc770925a84ad458fea8e6c
Joaquín <joaquinzini@gmail.com>
2021-02-12 10:17:36 -0300

    Creación de tabla con síntomas
    Co-authored-by: Wenceslao Mateos <wenceslaomateos@gmail.com>

--- Back-End/database/migrations/2021_02_11_111619_update_usuraios.php
+++ Back-End/database/migrations/2021_02_11_111619_update_usuraios.php
@@ -13,14 +13,25 @@ class UpdateUsuraios extends Migration
      */
     public function up()
     {
-            Schema::table('users', function (Blueprint $table) 
-            {
-                $table->year('birth_year');
-                $table->boolean('Tos_seca');
-                $table->boolean('Fiebre');
-            });
-  
-        //
+        Schema::table('users', function (Blueprint $table) 
+        {
+            $table->year('Fecha_Nacimiento');
+            $table->boolean('Tos_Seca');
+            $table->boolean('Fiebre');
+            $table->boolean('cansancio');
+            $table->boolean('Molestias_Cansancio');
+            $table->boolean('Dolor_De_Garganta');
+            $table->boolean('Diarrea');
+            $table->boolean('Conjuntivitis');
+            $table->boolean('Dolor_De_Cabeza');
+            $table->boolean('Pérdida_De_Olfato');
+            $table->boolean('Pérdida_De_Gusto');
+            $table->boolean('Erupciones_Cutáneas');
+            $table->boolean('Pérdida_Del_Control_En_Los_Dedos_De_Las_Manos');
+            $table->boolean('Dificultad_Para_Respirar');
+            $table->boolean('Incapacidad_Para_Moverse');
+            $table->boolean('Dolor/Presión_En_El_Pecho');
+        });
     }
 
     /**
@@ -32,9 +43,22 @@ class UpdateUsuraios extends Migration
     {
         schema::table('users', function (Blueprint $table)
         {
-            $table->dropColum('birth_year');
-            $table->dropColum('Tos_seca');
-            $table->dropColum('Fiebre');
-        });        //
+            $table->dropColumn('Fecha_Nacimiento');
+            $table->dropColumn('Tos_Seca');
+            $table->dropColumn('Fiebre');
+            $table->dropColumn('cansancio');
+            $table->dropColumn('Molestias_Cansancio');
+            $table->dropColumn('Dolor_De_Garganta');
+            $table->dropColumn('Diarrea');
+            $table->dropColumn('Conjuntuvitis');
+            $table->dropColumn('Dolor_De_Cabeza');
+            $table->dropColumn('Pérdida_De_Olfato');
+            $table->dropColumn('Pérdida_De_Gusto');
+            $table->dropColumn('Erupciones_Cutáneas');
+            $table->dropColumn('Pérdida_Del_Control_En_Los_Dedos');
+            $table->dropColumn('Dificultad_Para_Respirar');
+            $table->dropColumn('Incapacidad_Para_Moverse');
+            $table->dropColumn('Dolor/Presión_En_El_Pecho');
+            });        
     }
 }

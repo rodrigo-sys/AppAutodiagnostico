4ffeb1810f79062694bec9a8ce6f6eeeee975199
Wenceslao <wenceslaomatos@gmail.com>
2021-02-17 10:54:55 -0300

    Revert "Arreglos menores en Back-End"
    This reverts commit cb6731188b132dd8611556fc2e0f36efbfbf57fe.

--- Back-End/app/Http/Controllers/PersonaController.php
+++ Back-End/app/Http/Controllers/PersonaController.php
@@ -4,43 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Persona;
-use Illuminate\Support\Facades\DB;
+Use Illuminate\Support\Facades\DB;
 
 class PersonaController extends Controller
 {
 
-    public function index()
-    {
+    public function index(){
         //Selecciona todo lo que haya en la tabla persona y despues se muestra con el return
         $persona = Persona::all();
         return $persona;
+        
     }
 
-    function apellido()
-    {
+    function apellido(){
 
         //Selecciona lo que hay en apellido y lo obtiene, despues se muestra con el return
         $apellidoP = DB::table('personas')->select('apellido')->get();
         return $apellidoP;
+
     }
-    function sintomas()
-    {
+    function sintomas(){
 
         //Selecciona lo que hay en sintormas y lo obtiene, despues se muestra con el return
         $sintomasP = DB::table('personas')->select('sintomas')->get();
         return $sintomasP;
+
     }
-    function datos()
-    {
+    function datos(){
 
         $datoN = DB::table('personas')->select('nombre')->get();
         $datoA = DB::table('personas')->select('apellido')->get();
         return [$datoN, $datoA];
+
     }
-    function mostrarDatosPersonas(Request $request, $id)
-    {
+    function mostrarDatosPersonas(Request $request, $id){
         //No usar comillas para llamar las variables en la query
         $request = DB::table('personas')->where('id', '=', $id)->get();
         return $request;
     }
+    
+
+ 
 }
+
+
+
+
+
+

5ae4bfe552b8b159a3d16d6657c99bb2d9422a81
Wenceslao <wenceslaomatos@gmail.com>
2021-02-17 11:17:56 -0300

    Arreglos menores

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -6,15 +6,5 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
   ]
 })

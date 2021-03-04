e0d3b361ac202a0b07f1b0043f0ac746ddffa44e
Wenceslao <wenceslaomatos@gmail.com>
2021-03-04 09:12:35 -0300

    Arreglos menores

--- Front-End/src/resources/config.js
+++ Front-End/src/resources/config.js
@@ -7,14 +7,5 @@ const config = {
     JWT_URL: 'http://' + HOST + ':' + PORT + '/api/auth/',
     API_URL: 'http://' + HOST + ':' + PORT + '/api/',
 }
-
-var getJwtConfig = function() {
-    const token = localStorage.getItem('token');
-    return { headers: {Authorization: "Bearer " + token} };
-}
-
-module.exports = {
-    config: config,
-    getJwtConfig: getJwtConfig,
-}
+export default config;
     
